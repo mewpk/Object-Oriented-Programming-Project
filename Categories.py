0c9b10ee5ff7543b8e951b444344f612464df4a7
mewpk <milzaza123@gmail.com>
2023-03-20 23:17:39 +0700

    Successful Create class Categories in file Categories.py!

---
+++ Categories.py
@@ -0,0 +1,8 @@
+class Categories():
+    def __init__(self,id,name):
+        self.__id = id
+        self.__name = name
+    def request_catagories() : 
+        pass
+    def search_categories(keyword) : 
+        pass
