1c15e8e5fddb9a1e409f68580c1d3ca9f91c4630
24thofmayy <patthananchualam@gmail.com>
2023-03-21 00:08:18 +0700

    Successful create class Notification and create instance

---
+++ Notification.py
@@ -0,0 +1,13 @@
+class Notification():
+    def __init__(self,id,date,description):
+        self.id = id
+        self.date = date
+        self.description = description
+    def sent():
+        pass
+
+midyear_sale = Notification(id = "3345", date = "25-06-2022", description = "mid year sale start now!!!")
+christmas_sale = Notification(id = "2445", date = "25-12-2022", description = "christmas sale start now hohoho!!!")
+eleven_eleven_sale = Notification(id = "2342", date = "11-11-2022", description = "11.11 sale up to 50 percent")
+newyear_sale = Notification(id = "2456", date = "29-12-2022", description = "new year sale start now ")
+
