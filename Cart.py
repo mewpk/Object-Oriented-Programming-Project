571e08c5a3509455c9e4cee382d8d69b6fa1c924
24thofmayy <patthananchualam@gmail.com>
2023-03-21 00:09:08 +0700

    Successful create class Cart and create instance

---
+++ Cart.py
@@ -0,0 +1,14 @@
+class Cart():
+    def __init__(self,price,net_promotion,net_coupon,net_price):
+        self.price = price
+        self.net_promotion = net_promotion
+        self.net_coupon = net_coupon
+        self.net_price = net_price
+    def check_condition(coupon):
+        pass
+
+cart_1245 = (price = "1000", net_promotion = "20", net_coupon = "20", net_price = "600")
+cart_2345 = (price = "2344", net_promotion = "15", net_coupon = "30", net_price = "1290")
+cart_4634 = (price = "1034", net_promotion = "0", net_coupon = "40", net_price = "620")
+cart_5678 = (price = "5432", net_promotion = "25", net_coupon = "50", net_price = "1358")
+
