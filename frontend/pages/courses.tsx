038ba3128c56b004f974526f302ab43549989c49
MewPK <milzaza123@gmail.com>
2023-04-19 02:21:22 +0700

    connect frontend , backend show all course !

--- frontend/pages/courses.tsx
+++ frontend/pages/courses.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+
+import CourseList from "../components/CourseList";
+import { useEffect, useState } from "react";
 
 export default function Course() {
+  const [data, setData] = useState("");
+  const getData = async () => {
+    const res = await fetch("http://localhost:8000/course", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+    let dataRes = await res.json();
+    setData(dataRes);
+    console.log(dataRes);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
-    <div>Course</div>
+    <div  className="container mx-auto p-10">
+    <CourseList courses={data} />
+    </div>
   )
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,9 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 
 export default function Home() {
+
+
   return (
     <>
       <Head>
@@ -10,9 +12,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='container mx-auto'>
+      <main className="container mx-auto p-10">
         
       </main>
     </>
-  )
+  );
 }
