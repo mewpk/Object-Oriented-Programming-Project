3e1fcdeecfcadd0e1bd8046621639798ccf0f342
MewPK <milzaza123@gmail.com>
2023-04-18 22:53:14 +0700

    rename file

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
-from .routers import  Users, Course , Coupon
+from .routers import  Course , Coupon, Categories, users
 
 app = FastAPI()
 
-app.include_router(Users.router)
+app.include_router(users.router)
 app.include_router(Course.router)
 app.include_router(Coupon.router)
-
+app.include_router(Categories.router)
 @app.get("/")
 async def read_root():
     return {"message": "Yo!!! it's PW right here!!!!"}
